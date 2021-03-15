969ca4880775b3c18e50f6a94437bcbe77712d8e
melkwastaken <77582036+melkwastaken@users.noreply.github.com>
2021-03-15 10:49:20 +0000

    This will do, C++ hello world code will fix it.

---
+++ alrighthisfixit.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!";
+  return 0;
+}
+----
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
+-------
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  cout << "I am learning C++";
+  return 0;
+}
+---------
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! \n";
+  cout << "I am learning C++";
+  return 0;
+}
+------------
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! \n\n";
+  cout << "I am learning C++";
+  return 0;
+}
+-----------
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  cout << "I am learning C++";
+  return 0;
+}
+--------------
+/* The code below will print the words Hello World!
+to the screen, and it is amazing */
+cout << "Hello World!";
+--------------
+int myNum = 15;
+cout << myNum;
+-----------
